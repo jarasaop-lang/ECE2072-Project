2f43adab3fc88f9d63fbe361ad32e54df8ae98f0
Tevin Mudalige <tcolombamudalige@gmail.com>
2026-09-15 15:58:39 +1000

    2072quartus x72: Initial project upload

---
+++ components.v
@@ -0,0 +1,112 @@
+/*
+Monash University ECE2072: Assignment 
+This file contains Verilog code to implement individual components to be used in 
+    the CPU.
+
+Please enter your name and student ID:
+
+*/
+module sign_extend(in, ext);
+	/* 
+	 * This module sign extends the 9-bit Din to a 16-bit output.
+	 */
+	// TODO: Declare inputs and outputs
+	input [8:0] in;
+	output [15:0] ext;
+	
+	// TODO: implement logic
+	assign ext = {7{in[8]}, {in}};
+	
+endmodule
+
+
+
+module tick_FSM(rst, clk, enable, tick);
+	/* 
+	 * This module implements a tick FSM that will be used to
+	 * control the actions of the control unit
+	 */
+
+	// TODO: Declare inputs and outputs
+	input clk;
+	input rst;
+	input enable;
+	output reg [3:0] tick;
+	
+    // TODO: implement FSM
+	 
+	 parameter t1 = 4'b0001;
+	 
+	 always @(posedge clk) begin
+		 if (rst) tick <= t1;
+		 
+		 else begin
+		 
+			 tick <= {tick[2:0], tick[3]};
+		 end
+	 end S
+			 
+endmodule
+
+module multiplexer(SignExtDin, R0, R1, R2, R3, R4, R5, R6, R7, G, sel, Bus);
+	/* 
+	 * This module takes 10 inputs and places the correct input onto the bus.
+	 */
+	// TODO: Declare inputs and outputs
+	
+	// TODO: implement logic
+
+
+endmodule
+
+module ALU (input_a, input_b, alu_op, result);
+	/* 
+	 * This module implements the arithmetic logic unit of the processor.
+	 */
+	// TODO: declare inputs and outputs
+	input [15:0] input_a;
+	input [15:0] input_b;
+	input [2:0] alu_op;
+	output reg [15:0] result;	
+
+	// TODO: Implement ALU Logic:
+	parameter add = 3'b001, sub = 3'b010, mul = 3'b000, ss = 3'b011;   
+	always@(alu_op) begin
+		 case (alu_op) 
+		 
+			 (add):
+				 result = input_a + input_b;
+			 (sub):
+				 result = input_a - input_b;
+			 (mul):
+				 result = input_a * input_b;
+			 (ss):
+				 if (input_a[15]) begin
+					 s = $signed(input_a)*-1
+					 for(i=0, i<16, i=i+1) begin
+						 input_b[i<s] = 0;
+						 in
+			  
+		 
+endmodule
+
+
+
+module register_n(data_in, r_in, clk, Q, rst);
+
+
+	// To set parameter N during instantiation, you can use:
+	// register_n #(.N(num_bits)) reg_IR(.....), 
+	// where num_bits is how many bits you want to set N to
+	// and "..." is your usual input/output signals
+
+	parameter N = 16;
+
+	/* 
+	 * This module implements registers that will be used in the processor.
+	 */
+	// TODO: Declare inputs, outputs, and parameter:
+	
+	// TODO: Implement register logic:
+endmodule
+
